b1b001c1564b8672df59f04015ee3a0bbc44d459
NehGuk <nurture.tumulus_0n@icloud.com>
2026-01-27 13:02:13 +0100

    Formats all code with 2-space indent

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Kodehode
+
 My first project as a student at the Kodehode project.
--- scripts/jan26-javascript.js
+++ scripts/jan26-javascript.js
@@ -5,19 +5,17 @@ const changeNameButton = document.querySelector("#change-name-button")
 const url = "https://miku-api.vercel.app/mikuFacts"
 const mikuFactsSection = document.querySelector("miku-facts-section")
 
-changeNameButton.addEventListener("click", function() {
-    initialName.textContent = newName
-    changeNameButton.textContent = "Good job"
-    changeNameButton.setAttribute("disabled", "true")
-});
+changeNameButton.addEventListener("click", function () {
+  initialName.textContent = newName
+  changeNameButton.textContent = "Good job"
+  changeNameButton.setAttribute("disabled", "true")
+})
 
 async function getData(url) {
-    const response = await fetch(url)
-    let data = await response.json()
-    console.log(data)
-    console.log(data.length)
-    console.log(data[1])
+  const response = await fetch(url)
+  let data = await response.json()
+  console.log(data)
+  console.log(data.length)
+  console.log(data[1])
 }
 getData(url)
-
-
