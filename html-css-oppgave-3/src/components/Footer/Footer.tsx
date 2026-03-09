b7eec9dbf31afac3b1d35d433393deea7ad42829
NehGuk <nurture.tumulus_0n@icloud.com>
2026-03-09 22:25:03 +0100

    Adds resources and footer

--- html-css-oppgave-3/src/components/Footer/Footer.tsx
+++ html-css-oppgave-3/src/components/Footer/Footer.tsx
@@ -4,12 +4,7 @@ function Footer() {
   return (
     <footer className="footer" id="6">
       <h3>HTML & CSS | Oppgave 3</h3>
-      <p>By Henri</p>
-      <h3>Newsletter</h3>
-      <form>
-        <label htmlFor="email">Subscribe</label>
-        <input type="email" id="email" placeholder="name@email.com" />
-      </form>
+      <p>Kodehode</p>
     </footer>
   )
 }
--- html-css-oppgave-3/src/components/Resources/SectionResources.tsx
+++ html-css-oppgave-3/src/components/Resources/SectionResources.tsx
@@ -3,20 +3,44 @@ import "./SectionResources.css"
 function SectionResources() {
   return (
     <section className="resources" id="5">
-      <h2>Resources</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores
-        sapiente nulla hic nobis amet nihil ducimus a assumenda, quia, adipisci
-        voluptate consequatur at minima sed odio delectus blanditiis eligendi
-        numquam?
-      </p>
-
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores
-        sapiente nulla hic nobis amet nihil ducimus a assumenda, quia, adipisci
-        voluptate consequatur at minima sed odio delectus blanditiis eligendi
-        numquam?
-      </p>
+      <article className="resources_box">
+        <h2>Resources</h2>
+        <ul>
+          <li>
+            The European Space Agency, ESA |{" "}
+            <a
+              href="https://www.esa.int/Enabling_Support/Operations/Solar_system_exploration"
+              target="_blank"
+            >
+              Solar system exploration
+            </a>
+          </li>
+          <li>
+            NASA |{" "}
+            <a
+              href="https://science.nasa.gov/solar-system/planets/"
+              target="_blank"
+            >
+              About the planets
+            </a>
+          </li>
+          <li>
+            The Planetary Society |{" "}
+            <a href="https://www.planetary.org/worlds/mars" target="_blank">
+              Mars, the red planet
+            </a>
+          </li>
+          <li>
+            National Geographic |{" "}
+            <a
+              href="https://www.nationalgeographic.com/science/article/mars-1"
+              target="_blank"
+            >
+              Planet Mars, explained
+            </a>
+          </li>
+        </ul>
+      </article>
     </section>
   )
 }
