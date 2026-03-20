1c322661bfae9bd6b770f55a4c78290c6d66da50
NehGuk <nurture.tumulus_0n@icloud.com>
2026-03-20 23:20:59 +0100

    Drafts first random ideas on index.html

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Javascript | Oppgave 4
+
+- [Deployed at Netlify](js-oppgave-4.netlify.app)
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("heyyy")
+const tabCrashButton = document.querySelector("#crasher")
+
+function crashTab() {
+  for (let i = 0; i > -1; i++) {}
+}
+
+tabCrashButton.addEventListener("click", () => {
+  crashTab()
+})
